9c8b9ff01fd4cf7bb9fa12dac056964e255e178b
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-11 10:54:53 -0700

    text input styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,45 @@
-import { TextInput, View } from "react-native";
-
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return(
-        <View>
-            <TextInput/>
-            {/* Buttons */}
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.numberInput} maxLength={2}/>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     )
 
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        /* Elevation is an IOS property only */
+        elevation: 4,
+        marginHorizontal: 24,
+        marginTop: 100,
+        padding: 16,
+        /* Shadow is for IOS */
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.25,
+
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+
+    }
+
+})
